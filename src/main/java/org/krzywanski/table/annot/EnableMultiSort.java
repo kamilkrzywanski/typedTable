7ef2d33518015ec338ab6d10c4baf86eaca79e29
kam.krz <kamil.krzywanski@kalasoft.pl>
2023-12-05 14:16:30 +0100

    #Add multisort option

--- src/main/java/org/krzywanski/table/annot/EnableMultiSort.java
+++ src/main/java/org/krzywanski/table/annot/EnableMultiSort.java
@@ -4,6 +4,9 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * Flag to enable multi sort
+ */
 @Retention(RetentionPolicy.RUNTIME)
 @Target( { java.lang.annotation.ElementType.TYPE })
 public @interface EnableMultiSort {
