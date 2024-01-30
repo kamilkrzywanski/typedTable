c7adc242ddc5ba12e2bb20a4aee32a637bdeb4db
Kamil Krzywanski <kamilkrzywanski01@gmail.com>
2024-01-30 18:50:59 +0100

    #create repo for tests

---
+++ sql/create_tables.sql
@@ -0,0 +1,9 @@
+CREATE TABLE IF NOT EXISTS TestModel (
+    columnA VARCHAR(255),
+    columnB DECIMAL,
+    columnC VARCHAR(255),
+    date TIMESTAMP,
+    testEnum VARCHAR(255),
+    testFormatClass VARCHAR(255),
+    booleanValue BOOLEAN
+);
